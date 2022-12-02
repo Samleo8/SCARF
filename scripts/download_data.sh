3b588c84f7886d87b96c8a50cbdd49c56ca58a53
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-02 00:28:17 -0500

    update download script

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# clear old zip files before unzip
+rm data/Rectified.zip*
+
+# download data from internet
 wget http://roboimagedata2.compute.dtu.dk/data/MVS/Rectified.zip -P data/
 unzip data/Rectified.zip -d data/
 mv data/Rectified/* data/DTU_MVS
 rmdir data/Rectified
+
+# clear old zip files after unzip
+rm data/Rectified.zip*
