0918bcb23a389c4c78e7cc8529c024915b77d4e0
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-03 16:56:23 -0500

    open tb

--- scripts/tensorboard.sh
+++ scripts/tensorboard.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-tensorboard --logdir=logs/train_DTU/tensorboard --port=6006
+# This script is used to start tensorboard in a headless environment
+killall tensorboard
+nohup tensorboard --logdir=logs/train_DTU/tensorboard --port=6006 &>/dev/null &
+
+PID=$!
+echo "Tensorboard started in background (${PID}) at port 6006"
+echo ${PID} >.tb_pid
