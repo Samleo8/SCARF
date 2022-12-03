a27e4f8a6de323f57a6aaeca9f87d7fa9d6768e1
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-03 00:23:26 -0500

    force python 3  git script

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,4 +6,4 @@ echo "" > nohup.out
 NUM_PROC=$(nproc)
 echo "NOTE: Using $NUM_PROC workers (number of CPUs in VM)."
 
-python trainer.py --config configs/train_DTU.txt --num_workers=$NUM_PROC $@
+python3 trainer.py --config configs/train_DTU.txt --num_workers=$NUM_PROC $@
