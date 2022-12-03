00b1eae05e73f78e29e24ccb949a6baf2b3d5078
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-03 16:42:19 -0500

    update download log script

--- scripts/download_logs.sh
+++ scripts/download_logs.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
-INSTANCE=${1:-power}
+dl_log() {
+    LOG_TYPE=${1:-"all"}
+    INSTANCE=${2:-"power"}
+
+    case $LOG_TYPE in
+    "args" | "config" | "metadata")
+        gscpfrom ~/vl/project/stereo-nerf/logs/train_DTU/args.txt ./logs/train_DTU/ $INSTANCE
+        gscpfrom ~/vl/project/stereo-nerf/logs/train_DTU/config.txt ./logs/train_DTU/ $INSTANCE
+        ;;
+    "tensorboard")
+        gscpfrom ~/vl/project/stereo-nerf/logs/train_DTU/tensorboard ./logs/train_DTU/ $INSTANCE
+        ;;
+    "training_visualization" | "vis")
+        gscpfrom ~/vl/project/stereo-nerf/logs/train_DTU/training_visualization ./logs/train_DTU/ $INSTANCE
+        ;;
+    "all")
+        dl_log metadata
+        dl_log tensorboard
+        dl_log training_visualization
+        ;;
+    *)
+        dl_log "all"
+        ;;
+    esac
+}
+
+LOG_TYPE=${1:-all}
+INSTANCE=${2:-power}
 
 mkdir -p logs/train_DTU
 mkdir logs/train_DTU/training_visualization
+mkdir logs/train_DTU/tensorboard
 
-gscpfrom ~/vl/project/stereo-nerf/logs/train_DTU/args.txt ./logs/train_DTU/ $INSTANCE
-gscpfrom ~/vl/project/stereo-nerf/logs/train_DTU/config.txt ./logs/train_DTU/ $INSTANCE
-gscpfrom ~/vl/project/stereo-nerf/logs/train_DTU/training_visualization ./logs/train_DTU/training_visualization $INSTANCE
-gscpfrom ~/vl/project/stereo-nerf/logs/train_DTU/tensorboard ./logs/train_DTU/tensorboard $INSTANCE
+dl_log $LOG_TYPE $INSTANCE
