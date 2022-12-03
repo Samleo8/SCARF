906e74b7672f625592834660cc1b65ad5fafd0b3
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-03 00:05:56 -0500

    git pull n use number of processors

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-python trainer.py --config configs/train_DTU.txt $@
+git pull
+echo "" > nohup.out
+
+NUM_PROC=$(nproc)
+echo "NOTE: Using $NUM_PROC workers (number of CPUs in VM)."
+
+python trainer.py --config configs/train_DTU.txt --num_workers=$NUM_PROC $@
