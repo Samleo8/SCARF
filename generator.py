ce871d0bf1d49137cc54f0ab2c51f7cfdaf3f4a2
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-03 16:46:23 -0500

    add todo

--- generator.py
+++ generator.py
@@ -132,6 +132,7 @@ def render_and_save(i4d, dataset, render_data, savedir, img_outpath,
             imageio.imwrite(filename, (target * 255).numpy().astype(np.uint8))
 
     # Save rendered image, converting to uint8
+    # TODO: NOTE: added rgb * 255 here to fix warning about float values
     rgb = (rgb * 255).numpy().astype(np.uint8)
     imageio.imwrite(img_outpath, rgb)
 
