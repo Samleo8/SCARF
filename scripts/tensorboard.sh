3fb495b08af5179b86ffc18f04ca92589192c348
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-04 16:54:08 -0500

    allow to log at 2 different experiemnts

--- scripts/tensorboard.sh
+++ scripts/tensorboard.sh
@@ -3,8 +3,13 @@
 # This script is used to start tensorboard in a headless environment
 EXP_NAME=${1:-"train_DTU"}
 
+LOG_DIR=logs/${EXP_NAME}/tensorboard
+if [ $1 == "all" ]; then
+    LOG_DIR=logs
+fi
+
 killall tensorboard
-nohup tensorboard --logdir=logs/${EXP_NAME}/tensorboard --port=6006 &>/dev/null &
+nohup tensorboard --logdir=LOG_DIR --port=6006 &>/dev/null &
 
 PID=$!
 echo "Tensorboard started in background (${PID}) at port 6006"
