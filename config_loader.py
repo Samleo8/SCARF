21e2c632f9b0d90e79c521e064b0ab15608de8ae
Alex Strasser <arstrasser@gmail.com>
2022-12-04 01:49:37 -0500

    steal cnn parameters from another net

--- config_loader.py
+++ config_loader.py
@@ -30,6 +30,7 @@ def config_parser():
     parser.add_argument("--num_transformer_layers", type=int, default=2)
     parser.add_argument("--disable_pos_encoding", action='store_true')
 
+    parser.add_argument("--cnn_weight_path", type=str, default=None)
     parser.add_argument("--freze_cnn", action='store_true')
 
     # training options
--- model.py
+++ model.py
@@ -290,6 +290,25 @@ class Implicit4D():
             print('Current learning-rate: ',
                   self.optimizer.param_groups[0]['lr'])
             
+
+            conv_layers = ["conv_in", "conv_0", "conv_0_1", "conv_1", "conv_1_1", "conv_2", "conv_2_1", "conv_3", "conv_3_1", "conv_4", "conv_4_1", "conv_5", "conv_5_1", "fc_0", "fc_1"]
+            
+            if self.cfg.cnn_weight_path is not None:
+                cnn_model = torch.load(self.cfg.cnn_weight_path)
+                own_state = self.model.state_dict()
+                for name, param in cnn_model.items():
+                    if name not in conv_layers:
+                        continue
+                    if isinstance(param, Parameter):
+                        param = param.data
+                    try:
+                        own_state[name].copy_(param)
+                        print('Copied {}'.format(name))
+                    except:
+                        print('Did not find {}'.format(name))
+                        continue
+
+
             if self.cfg.freeze_cnn:
                 print('Freezing CNN Layers')
                 for layer in (self.model.conv_in,
