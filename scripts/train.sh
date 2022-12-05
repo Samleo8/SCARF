b9ada59b030c53115ede5d9ddaeae348085d3875
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-04 20:41:32 -0500

    update transformer layers

--- scripts/train.sh
+++ scripts/train.sh
@@ -7,4 +7,4 @@ echo "" > nohup.out
 # echo "NOTE: Using $NUM_PROC workers (number of CPUs in VM)."
 NUM_PROC=12
 
-python3 trainer.py --config configs/train_DTU4.txt --num_workers=$NUM_PROC --no_parallel $@
+python3 trainer.py --config configs/train_DTU_2L_16H.txt --num_workers=$NUM_PROC --no_parallel $@
