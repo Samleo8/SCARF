532440ce7bcb06cc8f4fc6a13c211d363afff6f9
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-04 20:04:56 -0500

    attempt to run multiple tensorboard again

--- scripts/tensorboard.sh
+++ scripts/tensorboard.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # This script is used to start tensorboard in a headless environment
-# EXP_NAME=${1:-"train_DTU"}
-# LOG_DIR="${EXP_NAME}:./logs/${EXP_NAME}/tensorboard"
-
-# while [ $# -gt 0 ]; do
-#     shift
-#     EXP_NAME=$1
-#     LOG_DIR="${LOG_DIR},${EXP_NAME}:./logs/${EXP_NAME}/tensorboard"
-# done
-
 EXP_NAME=${1:-"train_DTU"}
-LOG_DIR="./logs/${EXP_NAME}/tensorboard"
+LOG_DIR="${EXP_NAME}:./logs/${EXP_NAME}/tensorboard"
+
+while [ $# -gt 0 ]; do
+    shift
+    EXP_NAME=$1
+    LOG_DIR="${LOG_DIR},${EXP_NAME}:./logs/${EXP_NAME}/tensorboard"
+done
+
+# EXP_NAME=${1:-"train_DTU"}
+# LOG_DIR="./logs/${EXP_NAME}/tensorboard"
 
 killall tensorboard
 nohup tensorboard --logdir_spec=$LOG_DIR --port=6006 &>/dev/null &
