c0f1b3b71dfe98030250c4ec4fc3cf9862c84b62
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-05 02:08:32 -0500

    back to tensorboard single

--- scripts/tensorboard.sh
+++ scripts/tensorboard.sh
@@ -3,19 +3,19 @@
 # This script is used to start tensorboard in a headless environment
 EXP_NAME=${1:-"train_DTU"}
 
-LOG_DIR="${EXP_NAME}:./logs/${EXP_NAME}/tensorboard"
+# LOG_DIR="${EXP_NAME}:./logs/${EXP_NAME}/tensorboard"
 
-shift
-while [ $# -gt 0 ]; do
-    EXP_NAME=$1
-    LOG_DIR="${LOG_DIR},${EXP_NAME}:./logs/${EXP_NAME}/tensorboard"
-    shift
-done
+# shift
+# while [ $# -gt 0 ]; do
+#     EXP_NAME=$1
+#     LOG_DIR="${LOG_DIR},${EXP_NAME}:./logs/${EXP_NAME}/tensorboard"
+#     shift
+# done
 
-# LOG_DIR="./logs/${EXP_NAME}/tensorboard"
+LOG_DIR="./logs/${EXP_NAME}/tensorboard"
 
 killall tensorboard
-nohup tensorboard --logdir_spec=$LOG_DIR --port=6006 &>/dev/null &
+nohup tensorboard --logdir=$LOG_DIR --port=6006 &>/dev/null &
 
 PID=$!
 echo "Tensorboard started in background (${PID}) at port 6006"
