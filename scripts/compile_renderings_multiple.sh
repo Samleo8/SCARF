f6528f5cdd5a664c0b7038005a7accd62cdc04f4
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-07 02:00:58 -0500

    print message

--- scripts/compile_renderings_multiple.sh
+++ scripts/compile_renderings_multiple.sh
@@ -3,5 +3,6 @@
 EXPERIMENTS=(train_DTU train_DTU_2L_16H train_DTU_2L_nocompress)
 
 for EXP in ${EXPERIMENTS[@]}; do
+    echo "Compiling renderings for $EXP"
     ./scripts/compile_renderings.sh $EXP &
 done
