4a97eccfa8fdc900ee459fe5af6f4d5b06b3fb78
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-07 02:10:01 -0500

    change helper tests

--- scripts/gen_render_video.sh
+++ scripts/gen_render_video.sh
@@ -4,7 +4,7 @@ EXP_NAME=$1
 FPS=${2:-10}
 
 if [ -z $EXP_NAME ]; then
-    echo "USAGE: ./mp4-from-folder.sh <subfolder-output-name> [results-folder]"
+    echo "USAGE: ./mp4-from-folder.sh <EXP_NAME> [FPS]"
     exit
 fi
 
@@ -13,5 +13,4 @@ for SUBFOLDER in $RENDER_BASE_FOLDER/*; do
     echo "Processing $SUBFOLDER"
     RENDER_FOLDER=$SUBFOLDER
     ffmpeg -y -framerate $FPS -i $RENDER_FOLDER/%0005d.png -loop -1 -profile:v high -crf 28 -pix_fmt yuv420p $RENDER_FOLDER/render.mp4
-    cd -
 done
