af5746a85e7480738676b63cc23edd692af957fe
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-07 14:14:27 -0500

    force no parallel

--- scripts/render_finetune.sh
+++ scripts/render_finetune.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 EXP_NAME=${1:-"train_DTU_2L_16H"}
-python3 generator.py --config configs/finetune_scan23.txt --generate_specific_samples scan23 --gen_pose 0 --ckpt_expname ${EXP_NAME}
+python3 generator.py --config configs/finetune_scan23.txt --generate_specific_samples scan23 --gen_pose 0 --no_parallel --ckpt_expname ${EXP_NAME}
