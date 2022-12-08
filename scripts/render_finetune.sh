0ec19ea32ea3e6b3a1e25e242d5de1d49b650ae4
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-08 17:05:27 -0500

    use a single script with single config file for render

--- scripts/render_finetune.sh
+++ scripts/render_finetune.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 EXP_NAME=${1:-"train_DTU_2L_16H"}
-python3 generator.py --config configs/finetune_scan23.txt --generate_specific_samples scan23 --gen_pose 0 --no_parallel --ckpt_expname ${EXP_NAME}
+SAMPLE=${2:-"scan23"}
+
+python3 generator.py --config configs/render_experiment.txt --expname render_experiment_${SAMPLE} --generate_specific_samples ${SAMPLE} --gen_pose 0 --no_parallel --ckpt_expname ${EXP_NAME}
