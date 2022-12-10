4cf97ef7631d9ab0131c35db65741b5d0a2a57e4
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-10 14:59:37 -0500

    change train scripts

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,6 +5,6 @@ echo "" > nohup.out
 
 # NUM_PROC=$(nproc)
 # echo "NOTE: Using $NUM_PROC workers (number of CPUs in VM)."
-NUM_PROC=12
+NUM_PROC=16
 
-python3 trainer.py --config configs/train_DTU_2L_16H.txt --num_workers=$NUM_PROC --no_parallel $@
+python3 trainer.py --config configs/train_DTU_2L_32H.txt --num_workers=$NUM_PROC --no_parallel $@
--- scripts/train2.sh
+++ scripts/train2.sh
@@ -5,7 +5,7 @@ echo "" >nohup2.out
 
 # NUM_PROC=$(nproc)
 # echo "NOTE: Using $NUM_PROC workers (number of CPUs in VM)."
-NUM_PROC=8
+NUM_PROC=16
 
 export CUDA_VISIBLE_DEVICES=1
-python3 trainer.py --config configs/train_DTU_2L_nocompress.txt --num_workers=$NUM_PROC --no_parallel $@
+python3 trainer.py --config configs/train_DTU_4L_16H.txt --num_workers=$NUM_PROC --no_parallel $@
