2440579b711ff89b5b90324bbc00cea28919433a
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-10 15:03:51 -0500

    fix errors in scripts

--- scripts/train_headless2.sh
+++ scripts/train_headless2.sh
@@ -8,7 +8,7 @@ nohup ./scripts/train2.sh $@ &>nohup2.out &
 
 # save PID
 PID=$!
-echo "Parallel training (train2) started in background task ${PID}. To see the output, run 'tail -f nohup.out'"
+echo "Parallel training (train2) started in background task ${PID}. To see the output, run 'tail -f nohup2.out'"
 echo ${PID} >.train2_pid
 
 # check nohup
