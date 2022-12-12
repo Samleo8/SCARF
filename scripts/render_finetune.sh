bcd36760e8629cb640409ee459e9bf3f170a8c87
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-11 21:17:51 -0500

    fix issue with shifting

--- scripts/render_finetune.sh
+++ scripts/render_finetune.sh
@@ -4,5 +4,8 @@ EXP_NAME=${1:-"train_DTU_2L_16H"}
 SAMPLE=${2:-"scan23"}
 CUDA_VISIBLE_DEVICE=${3:-"0"}
 
-shift 3
-CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICE python3 generator.py --config configs/render_experiment.txt --expname render_experiment_${EXP_NAME} --generate_specific_samples ${SAMPLE} --gen_pose 0 --no_parallel --ckpt_expname ${EXP_NAME} $@
+shift
+shift
+shift
+
+CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICE python3 generator.py --config configs/render_experiment.txt --expname "render_experiment_${EXP_NAME}" --generate_specific_samples ${SAMPLE} --gen_pose 0 --no_parallel --ckpt_expname ${EXP_NAME} $@
