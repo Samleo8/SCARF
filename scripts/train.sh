119526f72c2ed860612ad81de515fbdbb3808a61
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-12 01:32:56 -0500

    continue training 4L 16H

--- scripts/train.sh
+++ scripts/train.sh
@@ -7,4 +7,4 @@ echo "" > nohup.out
 # echo "NOTE: Using $NUM_PROC workers (number of CPUs in VM)."
 NUM_PROC=16
 
-python3 trainer.py --config configs/train_DTU_2L_32H.txt --num_workers=$NUM_PROC --no_parallel $@
+python3 trainer.py --config configs/train_DTU_4L_16H.txt --num_workers=$NUM_PROC --no_parallel $@
