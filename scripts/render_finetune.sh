a5885d22118e37461ec485cde949ea00e65a6a09
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-11 20:00:41 -0500

    allow fine tune to take arguments

--- scripts/render_finetune.sh
+++ scripts/render_finetune.sh
@@ -4,4 +4,5 @@ EXP_NAME=${1:-"train_DTU_2L_16H"}
 SAMPLE=${2:-"scan23"}
 CUDA_VISIBLE_DEVICE=${3:-"0"}
 
-CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICE python3 generator.py --config configs/render_experiment.txt --expname render_experiment_${SAMPLE} --generate_specific_samples ${SAMPLE} --gen_pose 0 --no_parallel --ckpt_expname ${EXP_NAME}
+shift 3
+CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICE python3 generator.py --config configs/render_experiment.txt --expname render_experiment_${EXP_NAME} --generate_specific_samples ${SAMPLE} --gen_pose 0 --no_parallel --ckpt_expname ${EXP_NAME} $@
