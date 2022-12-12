aa8089e7fc1d55be00acf3dcac701e8000ac7929
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-12 00:49:49 -0500

    fix scp from aws

--- scripts/download_logs.sh
+++ scripts/download_logs.sh
@@ -12,19 +12,19 @@ dl_log() {
 
     case $LOG_TYPE in
     "args" | "config" | "metadata")
-        scpfrom ~/vl/project/stereo-nerf/logs/${EXP_NAME}/args.txt ./logs/${EXP_NAME}/ $INSTANCE
-        scpfrom ~/vl/project/stereo-nerf/logs/${EXP_NAME}/config.txt ./logs/${EXP_NAME}/ $INSTANCE
+        scpfrom SCARF/logs/${EXP_NAME}/args.txt ./logs/${EXP_NAME}/ $INSTANCE
+        scpfrom SCARF/logs/${EXP_NAME}/config.txt ./logs/${EXP_NAME}/ $INSTANCE
         ;;
     "tensorboard")
-        scpfrom ~/vl/project/stereo-nerf/logs/${EXP_NAME}/tensorboard ./logs/${EXP_NAME}/ $INSTANCE
+        scpfrom SCARF/logs/${EXP_NAME}/tensorboard ./logs/${EXP_NAME}/ $INSTANCE
         ;;
     "training_visualization" | "vis")
-        gscpfrom ~/vl/project/stereo-nerf/logs/${EXP_NAME}/training_visualization ./logs/${EXP_NAME}/ $INSTANCE
+        gscpfrom SCARF/logs/${EXP_NAME}/training_visualization ./logs/${EXP_NAME}/ $INSTANCE
         ;;
     "render" | "renderings")
         # Download renderings
         # NOTE: Need to run script ./scripts/compile_renderings from within system to do so properly
-        scpfrom ~/vl/project/stereo-nerf/logs/${EXP_NAME}/renderings ./logs/${EXP_NAME} $INSTANCE
+        scpfrom SCARF/logs/${EXP_NAME}/renderings ./logs/${EXP_NAME} $INSTANCE
         ;;
     "all")
         dl_log metadata
@@ -36,10 +36,10 @@ dl_log() {
         CKPT_ID=${LOG_TYPE%.tar}
         CKPT_ID=$(expr $CKPT_ID + 0)
         echo "Downloading checkpoint $CKPT_ID"
-        scpfrom ~/vl/project/stereo-nerf/logs/${EXP_NAME}/$LOG_TYPE ./logs/${EXP_NAME}/ $INSTANCE
+        scpfrom SCARF/logs/${EXP_NAME}/$LOG_TYPE ./logs/${EXP_NAME}/ $INSTANCE
 
         # Download the corresponding visualizations
-        scpfrom ~/vl/project/stereo-nerf/logs/${EXP_NAME}/training_visualization/epoch_${CKPT_ID}_scan* ./logs/${EXP_NAME}/training_visualization $INSTANCE
+        scpfrom SCARF/logs/${EXP_NAME}/training_visualization/epoch_${CKPT_ID}_scan* ./logs/${EXP_NAME}/training_visualization $INSTANCE
         ;;
     *)
         dl_log "all"
