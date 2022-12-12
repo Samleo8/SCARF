fd9c392f08844aa913f1a8ce14733fa26cb0d6f5
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-11 20:03:35 -0500

    update some experiment stuff

--- scripts/render_finetune_multiple.sh
+++ scripts/render_finetune_multiple.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-EXP_NAMES=(train_DTU_2L_32H train_DTU_4L_16H train_DTU_2L_16H_reduced)
+EXP_NAMES=(train_DTU_4L_16H train_DTU_2L_32H train_DTU_2L_16H_reduced)
 
 CNT=0
 PIDS=""
 for EXP_NAME in "${EXP_NAMES[@]}"; do
     echo "Processing $EXP_NAME on GPU $CNT"
 
-    ./scripts/render_finetune.sh ${EXP_NAME} "scan23" $CNT &
+    if [ $CNT -eq 0 ]; then
+        ./scripts/render_finetune.sh ${EXP_NAME} "scan23" $CNT &
+    else
+        ./scripts/render_finetune.sh ${EXP_NAME} "scan23" $CNT "--num_transformer_layers 4 --num_attn_heads 16" &
+    fi
     PIDS="$! $PIDS"
 
     CNT=$((CNT + 1))
