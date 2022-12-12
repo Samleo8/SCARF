de98b9b6b3c822437e0acd8836a4b7d10fbf9598
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-11 19:37:00 -0500

    allow rendering on multiple cuda devices

--- scripts/render_finetune.sh
+++ scripts/render_finetune.sh
@@ -2,5 +2,6 @@
 
 EXP_NAME=${1:-"train_DTU_2L_16H"}
 SAMPLE=${2:-"scan23"}
+CUDA_VISIBLE_DEVICE=${3:-"0"}
 
-python3 generator.py --config configs/render_experiment.txt --expname render_experiment_${SAMPLE} --generate_specific_samples ${SAMPLE} --gen_pose 0 --no_parallel --ckpt_expname ${EXP_NAME}
+CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICE python3 generator.py --config configs/render_experiment.txt --expname render_experiment_${SAMPLE} --generate_specific_samples ${SAMPLE} --gen_pose 0 --no_parallel --ckpt_expname ${EXP_NAME}
--- scripts/render_finetune_multiple.sh
+++ scripts/render_finetune_multiple.sh
@@ -2,12 +2,12 @@
 
 EXP_NAMES=(train_DTU_2L_32H train_DTU_4L_16H train_DTU_2L_16H_reduced)
 
-CNT=1
+CNT=0
 PIDS=""
 for EXP_NAME in "${EXP_NAMES[@]}"; do
     echo "Processing $EXP_NAME on GPU $CNT"
 
-    CUDA_VISIBLE_DEVICES=$CNT python3 generator.py --config configs/finetune_scan23.txt --generate_specific_samples scan23 --gen_pose 0 --no_parallel --ckpt_expname ${EXP_NAME} &
+    ./scripts/render_finetune.sh ${EXP_NAME} "scan23" $CNT &
     PIDS="$! $PIDS"
 
     CNT=$((CNT + 1))
