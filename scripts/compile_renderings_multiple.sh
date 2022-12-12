ab07156d2645f88b86179b66ad46914371e08eff
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-12 03:49:41 -0500

    compile stuff

--- scripts/compile_renderings_multiple.sh
+++ scripts/compile_renderings_multiple.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-EXPERIMENTS=(train_DTU_2L_32H train_DTU_4L_16H train_DTU_2L_16H_reduced)
+EXPERIMENTS=(train_DTU_4L_16H train_DTU_2L_16H_reduced2)
 
 for EXP in ${EXPERIMENTS[@]}; do
     echo "Compiling renderings for $EXP"
--- scripts/render_finetune_multiple.sh
+++ scripts/render_finetune_multiple.sh
@@ -2,7 +2,7 @@
 
 EXP_NAMES=(train_DTU_4L_16H train_DTU_2L_16H_reduced2)
 
-CNT=1
+CNT=0
 PIDS=""
 for EXP_NAME in "${EXP_NAMES[@]}"; do
     echo "Processing $EXP_NAME on GPU $CNT"
