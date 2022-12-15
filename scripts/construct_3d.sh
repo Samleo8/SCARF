e32b5179985c8b860e07695eb7b147ade73c17a9
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-15 14:53:18 -0500

    fix script outputs and shifts

--- scripts/construct_3d.sh
+++ scripts/construct_3d.sh
@@ -2,8 +2,7 @@
 
 EXP_NAME=${1:-"train_DTU_2L_32H"}
 SCAN_NUM=${2:-23}
-POSE=${3:-"0"}
-CUDA_VISIBLE_DEVICE=${4:-"0"}
+CUDA_VISIBLE_DEVICE=${3:-"0"}
 
 shift
 shift
@@ -11,4 +10,4 @@ shift
 
 echo "Performing 3D reconstruction of scan $SCAN_NUM for $EXP_NAME on GPU $CUDA_VISIBLE_DEVICE with flags $@"
 
-CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICE python3 3d_reconstruction.py --config configs/render_experiment.txt --expname "render_${EXP_NAME}" --generate_specific_samples scan${SCAN_NUM} --gen_pose $POSE --no_parallel --ckpt_expname ${EXP_NAME} $@
+CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICE python3 3d_reconstruction.py --config configs/render_experiment.txt --expname "render_${EXP_NAME}" --generate_specific_samples scan${SCAN_NUM} --no_parallel --ckpt_expname ${EXP_NAME} $@
--- scripts/render_finetune.sh
+++ scripts/render_finetune.sh
@@ -8,7 +8,8 @@ CUDA_VISIBLE_DEVICE=${4:-"0"}
 shift
 shift
 shift
+shift
 
-echo "Rendering scan $SCAN_NUM for $EXP_NAME on GPU $CUDA_VISIBLE_DEVICE with flags $@"
+echo "Rendering scan $SCAN_NUM pose $POSE for $EXP_NAME on GPU $CUDA_VISIBLE_DEVICE with flags $@"
 
 CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICE python3 generator.py --config configs/render_experiment.txt --expname "render_${EXP_NAME}" --generate_specific_samples scan${SCAN_NUM} --gen_pose $POSE --no_parallel --ckpt_expname ${EXP_NAME} $@
