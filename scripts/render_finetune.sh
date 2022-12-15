cb9605c1f3df38ef9a7bb14c99eb01af0e34d251
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-15 14:09:34 -0500

    rename variables, change defaults

--- scripts/render_finetune.sh
+++ scripts/render_finetune.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-EXP_NAME=${1:-"train_DTU_2L_16H"}
-SAMPLE=${2:-23}
+EXP_NAME=${1:-"train_DTU_2L_32H"}
+SCAN_NUM=${2:-23}
 POSE=${3:-"0"}
 CUDA_VISIBLE_DEVICE=${4:-"0"}
 
@@ -9,6 +9,6 @@ shift
 shift
 shift
 
-echo "Rendering $SAMPLE for $EXP_NAME on GPU $CUDA_VISIBLE_DEVICE with flags $@"
+echo "Rendering scan $SCAN_NUM for $EXP_NAME on GPU $CUDA_VISIBLE_DEVICE with flags $@"
 
-CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICE python3 generator.py --config configs/render_experiment.txt --expname "render_${EXP_NAME}" --generate_specific_samples scan${SAMPLE} --gen_pose $POSE --no_parallel --ckpt_expname ${EXP_NAME} $@
+CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICE python3 generator.py --config configs/render_experiment.txt --expname "render_${EXP_NAME}" --generate_specific_samples scan${SCAN_NUM} --gen_pose $POSE --no_parallel --ckpt_expname ${EXP_NAME} $@
