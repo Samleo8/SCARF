219bde4cd48da0a23754dc15941a759baae4a3be
Samuel Leong <leowhiz@yahoo.com.sg>
2022-12-15 13:41:47 -0500

    change script run method

--- scripts/render_finetune.sh
+++ scripts/render_finetune.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 EXP_NAME=${1:-"train_DTU_2L_16H"}
-SAMPLE=${2:-"scan23"}
+SAMPLE=${2:-23}
 POSE=${3:-"0"}
 CUDA_VISIBLE_DEVICE=${4:-"0"}
 
@@ -11,4 +11,4 @@ shift
 
 echo "Rendering $SAMPLE for $EXP_NAME on GPU $CUDA_VISIBLE_DEVICE with flags $@"
 
-CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICE python3 generator.py --config configs/render_experiment.txt --expname "render_${EXP_NAME}" --generate_specific_samples ${SAMPLE} --gen_pose $POSE --no_parallel --ckpt_expname ${EXP_NAME} $@
+CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICE python3 generator.py --config configs/render_experiment.txt --expname "render_${EXP_NAME}" --generate_specific_samples scan${SAMPLE} --gen_pose $POSE --no_parallel --ckpt_expname ${EXP_NAME} $@
